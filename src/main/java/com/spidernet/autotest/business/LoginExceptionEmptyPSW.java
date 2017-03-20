ce4e9d2bc9bee8ed26db907b492b8cabf7873963
Jennie <jennie_xin@163.com>
2017-03-20 18:00:08 +0800

    junit update & EmptyPSW case & Register changes

--- src/main/java/com/spidernet/autotest/business/LoginExceptionEmptyPSW.java
+++ src/main/java/com/spidernet/autotest/business/LoginExceptionEmptyPSW.java
@@ -1,11 +1,73 @@
 package com.spidernet.autotest.business;
 
 import org.openqa.selenium.WebDriver;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import com.spidernet.autotest.util.CommonUtils;
+import com.spidernet.autotest.util.ConfigFile;
 
 public class LoginExceptionEmptyPSW implements IBusiness {
 	
 	public boolean excute(WebDriver driver) throws Exception{
-		return false;
+		
+		driver.navigate().to(ConfigFile.getConfigProperties("URL"));
+		
+		ConfigFile.appendContentToLogFile("***** CASE loginExceptionEmptyPSW ***** ");
+		
+		String currentURL1 = driver.getCurrentUrl();
+		ConfigFile.appendContentToLogFile("Opening logon page with URL: ");
+		ConfigFile.appendContentToLogFile(currentURL1);
+		
+		if (CommonUtils.isElementPresent(driver,By.xpath(ConfigFile.getElementProperties("logonButton"))))
+		{
+			WebElement logoelement1 = driver.findElement(By.xpath(ConfigFile.getElementProperties("logonButton")));
+			
+			if(logoelement1.getText().equals("登录"))
+			{
+				ConfigFile.appendContentToLogFile("Setp1: The logon page is opened successfully");
+			}
+			
+			//Maximize the browser
+			driver.manage().window().maximize();
+			
+			//Find the element of name and put into it
+			driver.findElement(By.xpath(ConfigFile.getElementProperties("username"))).sendKeys(ConfigFile.getValueProperties("username5"));
+			
+			ConfigFile.appendContentToLogFile("Testing with username5:");
+			ConfigFile.appendContentToLogFile(ConfigFile.getValueProperties("username5"));
+			
+			//Find the element of password and put into it.
+			driver.findElement(By.xpath(ConfigFile.getElementProperties("password"))).sendKeys(ConfigFile.getValueProperties("password5"));
+			
+			ConfigFile.appendContentToLogFile("Testing with password5:");
+			ConfigFile.appendContentToLogFile(ConfigFile.getValueProperties("password5"));
+		
+			//Click on the logon button
+			driver.findElement(By.xpath(ConfigFile.getElementProperties("logonButton"))).click();
+
+			//Put a Implicit wait, will wait for 10 seconds before throwing verification
+			driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+			
+			
+			
+			if(CommonUtils.isElementPresent(driver,By.xpath(ConfigFile.getElementProperties("inputPSW"))))
+			{
+				WebElement logoelement2 = driver.findElement(By.xpath(ConfigFile.getElementProperties("inputPSW")));
+				ConfigFile.appendContentToLogFile(logoelement2.getText());
+				return true;
+			}
+			
+			else 
+			{
+				return false;
+			}
+		}
+			
+		else			
+		{
+			return false;
+		}
 	}
 
 }
---
+++ src/test/java/com/spidernet/autotest/business/TestLoginPage.java
@@ -0,0 +1,70 @@
+package com.spidernet.autotest.business;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+import com.spidernet.autotest.business.IBusiness;
+import com.spidernet.autotest.business.LoginExceptionEmptyName;
+import com.spidernet.autotest.business.LoginExceptionInvalid;
+import com.spidernet.autotest.business.LoginNormal;
+import com.spidernet.autotest.util.CommonUtils;
+import com.spidernet.autotest.util.ConfigFile;
+
+public class TestLoginPage {
+	
+static WebDriver driver = null;
+	
+	@BeforeClass
+	public static void createDriver()  {
+		try{
+			driver = CommonUtils.createDriver(driver);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@AfterClass
+	public static void closeDriver() {
+		CommonUtils.closeDriver(driver);		
+	}
+
+	@Test
+	public void testExcute() {
+		try{
+			IBusiness loginExceptionInvalid = new LoginExceptionInvalid();
+			IBusiness loginExceptionEmptyName = new LoginExceptionEmptyName();
+			IBusiness loginExceptionEmptyPSW = new LoginExceptionEmptyPSW();
+			LoginNormal loginNormal = new LoginNormal();
+			//loginNormal.setUserName(userName);
+			
+			List<IBusiness> list = new ArrayList<IBusiness>();
+			list.add(loginExceptionEmptyName);
+			list.add(loginExceptionEmptyPSW);
+			list.add(loginExceptionInvalid);
+			list.add(loginNormal);
+			
+			for(IBusiness object:list){
+				boolean result = object.excute(driver);
+				if(result){
+					ConfigFile.appendContentToLogFile("******* Case Pass :) ******* ");
+					Assert.assertTrue(result);
+				}
+				else
+				{
+					ConfigFile.appendContentToLogFile("******* Case Fail :( ******* ");
+				}	
+				Thread.sleep(1000);
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+}
