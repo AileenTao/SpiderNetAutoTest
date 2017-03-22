81619f63fe9a124b81ab13f6c9fe2eeeadf76979
Jennie <jennie_xin@163.com>
2017-03-22 16:17:53 +0800

    adding register exception case for input format

--- src/test/java/com/spidernet/autotest/business/TestRegisterPage.java
+++ src/test/java/com/spidernet/autotest/business/TestRegisterPage.java
@@ -32,94 +32,129 @@ public class TestRegisterPage {
 		CommonUtils.closeDriver(driver);		
 	}
 
-	@Test
-	public void testRegisterNormal() {
-		try{
-			IBusiness loginNormal = new LoginNormal();
-			IBusiness registerNormal = new RegisterNormal();
-			IBusiness logOff = new LogOff();
-			LoginNormal loginNormal2 = new LoginNormal();
-
-			loginNormal2.setUserName(ConfigFile.getValueProperties("erNumber"));
-			loginNormal2.setPassword(ConfigFile.getValueProperties("empPassowrd"));
-			loginNormal2.setChineseName(ConfigFile.getValueProperties("chineseName"));
-			
-			List<IBusiness> list = new ArrayList<IBusiness>();
-
-			list.add(loginNormal);
-			list.add(registerNormal);
-			list.add(logOff);
-			list.add(loginNormal2);
-			list.add(logOff);
-				
-			for(IBusiness object:list){
-				boolean result = object.excute(driver);
-				if(result){
-					ConfigFile.appendContentToLogFile("******* Case Pass :) ******* ");
-					Assert.assertTrue(result);
-				}
-				else
-				{
-					ConfigFile.appendContentToLogFile("******* Case Fail :( ******* ");
-				}	
-				Thread.sleep(1000);
-			}
-			
-			
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		
-	}
+//	@Test
+//	public void testRegisterNormal() {
+//		try{
+//			IBusiness loginNormal = new LoginNormal();
+//			IBusiness registerNormal = new RegisterNormal();
+//			IBusiness logOff = new LogOff();
+//			LoginNormal loginNormal2 = new LoginNormal();
+//
+//			loginNormal2.setUserName(ConfigFile.getValueProperties("erNumber"));
+//			loginNormal2.setPassword(ConfigFile.getValueProperties("empPassowrd"));
+//			loginNormal2.setChineseName(ConfigFile.getValueProperties("chineseName"));
+//			
+//			List<IBusiness> list = new ArrayList<IBusiness>();
+//
+//			list.add(loginNormal);
+//			list.add(registerNormal);
+//			list.add(logOff);
+//			list.add(loginNormal2);
+//			list.add(logOff);
+//				
+//			for(IBusiness object:list){
+//				boolean result = object.excute(driver);
+//				if(result){
+//					ConfigFile.appendContentToLogFile("******* Case Pass :) ******* ");
+//					Assert.assertTrue(result);
+//				}
+//				else
+//				{
+//					ConfigFile.appendContentToLogFile("******* Case Fail :( ******* ");
+//				}	
+//				Thread.sleep(1000);
+//			}
+//			
+//			
+//		}catch(Exception e){
+//			e.printStackTrace();
+//		}
+//		
+//	}
+//	
+//	
+//	@Test
+//	public void testRegisterExceptionMessage() {
+//		try{
+//			IBusiness loginNormal = new LoginNormal();
+//		    IBusiness registerExceptionMessage = new RegisterExceptionMessage();
+//			IBusiness logOff = new LogOff();
+//				
+//			
+//			List<IBusiness> list = new ArrayList<IBusiness>();
+//
+//			list.add(loginNormal);
+//			list.add(registerExceptionMessage);
+//			list.add(logOff);
+//							
+//			for(IBusiness object:list){
+//				boolean result = object.excute(driver);
+//				if(result){
+//					ConfigFile.appendContentToLogFile("******* Case Pass :) ******* ");
+//					Assert.assertTrue(result);
+//				}
+//				else
+//				{
+//					ConfigFile.appendContentToLogFile("******* Case Fail :( ******* ");
+//				}	
+//				Thread.sleep(1000);
+//			}
+//			
+//			
+//		}catch(Exception e){
+//			e.printStackTrace();
+//		}
+//		
+//	}
+//
+//
+//	@Test
+//	public void testRegisterExceptionExistedUser() {
+//		try{
+//			LoginNormal loginNormal = new LoginNormal();
+//			IBusiness registerExceptionExistedUser = new RegisterExceptionExistedUser();
+//			IBusiness logOff = new LogOff();
+//			
+//		
+//			List<IBusiness> list = new ArrayList<IBusiness>();
+//
+//			list.add(loginNormal);
+//			list.add(registerExceptionExistedUser);
+//			list.add(logOff);
+//
+//				
+//			for(IBusiness object:list){
+//				boolean result = object.excute(driver);
+//				if(result){
+//					ConfigFile.appendContentToLogFile("******* Case Pass :) ******* ");
+//					Assert.assertTrue(result);
+//				}
+//				else
+//				{
+//					ConfigFile.appendContentToLogFile("******* Case Fail :( ******* ");
+//				}	
+//				Thread.sleep(1000);
+//			}
+//			
+//			
+//		}catch(Exception e){
+//			e.printStackTrace();
+//		}
+//		
+//	}
 	
-	
 	@Test
-	public void testRegisterExceptionMessage() {
-		try{
-			IBusiness loginNormal = new LoginNormal();
-		    IBusiness registerExceptionMessage = new RegisterExceptionMessage();
-			IBusiness logOff = new LogOff();
-				
-			
-			List<IBusiness> list = new ArrayList<IBusiness>();
-
-			list.add(loginNormal);
-			list.add(registerExceptionMessage);
-			list.add(logOff);
-							
-			for(IBusiness object:list){
-				boolean result = object.excute(driver);
-				if(result){
-					ConfigFile.appendContentToLogFile("******* Case Pass :) ******* ");
-					Assert.assertTrue(result);
-				}
-				else
-				{
-					ConfigFile.appendContentToLogFile("******* Case Fail :( ******* ");
-				}	
-				Thread.sleep(1000);
-			}
-			
-			
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		
-	}
-
-
-	@Test
-	public void testRegisterExceptionExistedUser() {
+	public void testRegisterExceptionInputFormat() {
 		try{
 			LoginNormal loginNormal = new LoginNormal();
-			IBusiness registerExceptionExistedUser = new RegisterExceptionExistedUser();
+			IBusiness registerExceptionInputFormat = new RegisterExceptionInputFormat();
 			IBusiness logOff = new LogOff();
 			
 		
 			List<IBusiness> list = new ArrayList<IBusiness>();
 
 			list.add(loginNormal);
-			list.add(registerExceptionExistedUser);
+			list.add(registerExceptionInputFormat);
 			list.add(logOff);
 
 				
